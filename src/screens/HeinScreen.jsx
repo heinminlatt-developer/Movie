5531e41191e0d2cf86582f673f74df1c482dd81a
Hein Min Latt <heinminlatt.ygn001@gmail.com>
2024-05-15 15:55:16 +0630

    HHHHHHH

---
+++ src/screens/HeinScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HeinScreen = () => {
+  return (
+    <View>
+      <Text>HeinScreen</Text>
+    </View>
+  )
+}
+
+export default HeinScreen
+
+const styles = StyleSheet.create({})
